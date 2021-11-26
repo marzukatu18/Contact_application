b36c285a84c587f05343b6627e977fd1019348ab
FlutterMe <74610483+FlutterMe@users.noreply.github.com>
2021-11-26 16:10:53 +0000

    contact data was added

--- lib/contact_model.dart
+++ lib/contact_model.dart
@@ -1,17 +1,17 @@
 // To parse this JSON data, do
 //
-//     final contact = contactFromMap(jsonString);
+//     final contact = contactFromJson(jsonString);
 
 import 'dart:convert';
 
-Contact contactFromMap(String str) => Contact.fromMap(json.decode(str));
+Contact contactFromJson(String str) => Contact.fromJson(json.decode(str));
 
-String contactToMap(Contact data) => json.encode(data.toMap());
+String contactToJson(Contact data) => json.encode(data.toJson());
 
 class Contact {
     Contact({
        required this.name,
-       required this.phone,
+      required this.phone,
        required this.email,
        required this.country,
        required this.region,
@@ -23,7 +23,7 @@ class Contact {
     String country;
     String region;
 
-    factory Contact.fromMap(Map<String, dynamic> json) => Contact(
+    factory Contact.fromJson(Map<String, dynamic> json) => Contact(
         name: json["name"],
         phone: json["phone"],
         email: json["email"],
@@ -31,7 +31,7 @@ class Contact {
         region: json["region"],
     );
 
-    Map<String, dynamic> toMap() => {
+    Map<String, dynamic> toJson() => {
         "name": name,
         "phone": phone,
         "email": email,
